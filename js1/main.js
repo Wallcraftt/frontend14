01be93d14bebdfaa6f713bbbd58916cff14dd167
Wallcraft <102182020+Wallcraftt@users.noreply.github.com>
2022-10-26 16:59:32 +0400

    added js

---
+++ js1/main.js
@@ -0,0 +1,32 @@
+let arithmeticAverage = [2,4,6,8,10];
+
+console.log(arithmeticAverage);
+console.log(arithmeticAverage[2]); 
+
+
+let user = {name: 'giorgi' , age: 27}
+
+console.log(user)
+
+
+const newUser = {
+  firstName: 'giorgi',
+  lastName:  'giorgadze',
+  age: 27,
+  phoneNumber: 598101529,
+  addres: 'Tbilisi',
+  zipcode: 110011,
+}
+
+console.log(newUser)
+
+console.log('User name is',newUser.firstName)
+console.log('User age is',newUser.age)
+console.log ('User address is',newUser.addres)
+
+
+
+
+console.table(newUser)
+console.error(newUser.zipcode)
+alert('reload')
